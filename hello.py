c67e076ec0326cca4c3c68ad6e3c96c8a50b9893
MariamDvalishvili <mariamidvalishvili@gmail.com>
2024-11-14 20:07:55 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,28 @@
-print("Hello, World!")
+print("see")
+
+################
+#Syntax
+
+if 3 > 5:
+ print("you are welcome")
+else:
+ print("you are right")
+______
+
+x = 6
+y = "Love!"
+
+print(x)
+print(y)
+print("x")
+print("y")
+_________
+
+a = 9
+b = 8
+if a == b:
+ Print ("All good")
+elif a < b:
+ print("Wrong")
+else:
+ print("Hi John")
