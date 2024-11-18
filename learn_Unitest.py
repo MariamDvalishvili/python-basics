7319afa8e41893f2c8602d0bea1a4633e0676288
MariamDvalishvili <mariamidvalishvili@gmail.com>
2024-11-18 22:10:08 +0100

    Create learn_Unitest.py

---
+++ learn_Unitest.py
@@ -0,0 +1,27 @@
+def multiplication(x, y):
+ return x * y
+
+sume=multiplication(9, 7)
+ 
+result = "The result = " + str(sume)
+print(result)
+
+####
+
+def minus(c, d):
+ return c - d
+ 
+sume=minus(198, 64)
+
+result = "The result = " + str(sume)
+print(result)
+
+####
+
+def add(z, a):
+ return z + a
+
+sume=add(90, 678)
+
+result = "The result = " + str(sume)
+print(result)
