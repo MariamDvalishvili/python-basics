b8efd3802491d78fa7a5b6181e70c5fef0bae827
MariamDvalishvili <mariamidvalishvili@gmail.com>
2024-11-19 22:12:39 +0100

    Update learn_Unitest.py

--- learn_Unitest.py
+++ learn_Unitest.py
@@ -1,3 +1,5 @@
+import unittest
+
 def multiplication(x, y):
  return x * y
 
@@ -25,3 +27,17 @@ sume=add(90, 678)
 
 result = "The result = " + str(sume)
 print(result)
+
+
+class TestMathFunctions(unittest.TestCase):
+    def test_multiplication(self):
+        self.assertEqual(multiplication(9, 7), 63, "Multiplication failed")
+
+    def test_minus(self):
+     self.assertEqual(minus(198, 64), 134, "minus failed")
+
+    def test_add(self):
+     self.assertEqual(add(90, 678), 768, "add failed")
+     
+if __name__ == "__main__":
+    unittest.main()
